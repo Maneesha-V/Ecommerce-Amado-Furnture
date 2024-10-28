5311a30ec8831d51a7d9f4ea0a70670bdf7316ba
Ubuntu <ubuntu@ip-172-31-90-189.ec2.internal>
2024-10-28 06:34:34 +0000

    recent db connectio changes

--- config/db.js
+++ config/db.js
@@ -6,25 +6,25 @@ const mongoDBUrl = process.env.MONGODB_URL;
 
 function connectDB() {
     mongoose.set('strictQuery', true);
-    mongoose.connect(mongoDBUrl, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 15000,
-    }, (err) => {
-        if (err) {
-            console.log('Error connecting to MongoDB:', err);
-        } else {
-            console.log('Connected to MongoDB');
-        }
-    });
-    // try{
-    //     mongoose.connect('mongodb+srv://manudaffodils13:j2oxYij52lPtQQMY@ecomwebapp.tm576.mongodb.net/ecom-furniture?retryWrites=true&w=majority&appName=ecomwebapp',{
-    //         useNewUrlParser:true
-    //     });
+   // mongoose.connect(mongoDBUrl, {
+     //   useNewUrlParser: true,
+       // useUnifiedTopology: true,
+       // serverSelectionTimeoutMS: 15000,
+   // }, (err) => {
+     //   if (err) {
+         //   console.log('Error connecting to MongoDB:', err);
+       // } else {
+        //    console.log('Connected to MongoDB');
+       // }
+//    });
+     try{
+         mongoose.connect('mongodb+srv://manudaffodils13:j2oxYij52lPtQQMY@ecomwebapp.tm576.mongodb.net/ecom-furniture?retryWrites=true&w=majority&appName=ecomwebapp',{
+             useNewUrlParser:true
+         });
         
-    // } catch (error) {
-    //     return res.status(500).send({status: "error", message: ''+err});
-    // }
+     } catch (error) {
+         return res.status(500).send({status: "error", message: ''+err});
+     }
 }
 
 module.exports = connectDB;
